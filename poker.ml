0058c48d4fd451319755b3ca94e80d07194f1425
Wyatt Marshall <wgm46@cornell.edu>
2020-04-18 15:43:42 -0500

    Edited our stuff

--- poker.ml
+++ poker.ml
@@ -1,49 +1,43 @@
-type suit =
-  |Hearts
-  |Diamonds
-  |Clubs
-  |Spades
-
-type card = 
-  |Two of suit  
-  |Three of suit
-  |Four of suit
-  |Five of suit
-  |Six of suit
-  |Seven of suit
-  |Eight of suit
-  |Nine of suit
-  |Ten of suit
-  |Jack of suit
-  |Queen of suit
-  |King of suit
-  |Ace of suit
+type card =
+  |Hearts of int
+  |Diamonds of int
+  |Clubs of int 
+  |Spades of int
 
 type deck = card array
 type hand = card array
+type table = card array
 
-let full_deck = Array.of_list [Two Hearts; Two Diamonds; Two Clubs; Two Spades;
-                               Three Hearts; Three Diamonds; Three Clubs; Three Spades; 
-                               Four Hearts; Four Diamonds; Four Clubs; Four Spades;
-                               Five Hearts; Five Diamonds; Five Clubs; Five Spades;
-                               Six Hearts; Six Diamonds; Six Clubs; Six Spades;
-                               Seven Hearts; Seven Diamonds; Seven Clubs; Seven Spades;
-                               Eight Hearts; Eight Diamonds; Eight Clubs; Eight Spades;
-                               Nine Hearts; Nine Diamonds; Nine Clubs; Nine Spades;
-                               Ten Hearts; Ten Diamonds; Ten Clubs; Ten Spades;
-                               Jack Hearts; Jack Diamonds; Jack Clubs; Jack Spades;
-                               Queen Hearts; Queen Diamonds; Queen Clubs; Queen Spades;
-                               King Hearts; King Diamonds; King Clubs; King Spades;
-                               Ace Hearts; Ace Diamonds; Ace Clubs; Ace Spades;]
+let full_deck = Array.of_list [Hearts 0 ; Hearts 1 ; Hearts 2 ; Hearts 3 ; Hearts 4 ; Hearts 5 ; Hearts 6
+                              ; Hearts 7 ; Hearts 8 ; Hearts 9 ; Hearts 10 ; Hearts 11 ; Hearts 12 ;
+                               Diamonds 0 ; Diamonds 1 ; Diamonds 2 ; Diamonds 3 ; Diamonds 4 ; Diamonds 5 ; Diamonds 6
+                              ; Diamonds 7 ; Diamonds 8 ; Diamonds 9 ; Diamonds 10 ; Diamonds 11 ; Diamonds 12 ;
+                               Spades 0 ; Spades 1 ; Spades 2 ; Spades 3 ; Spades 4 ; Spades 5 ; Spades 6
+                              ; Spades 7 ; Spades 8 ; Spades 9 ; Spades 10 ; Spades 11 ; Spades 12 ;
+                               Clubs 0 ; Clubs 1 ; Clubs 2 ; Clubs 3 ; Clubs 4 ; Clubs 5 ; Clubs 6
+                              ; Clubs 7 ; Clubs 8 ; Clubs 9 ; Clubs 10 ; Clubs 11 ; Clubs 12 ;]
 
 let rec shuffle (fulldeck: card array) (acc:deck) : deck = 
   if Array.length acc = 9 then acc else
-    let pos = Random.int 52 in 
-    shuffle (Array.concat [(Array.sub fulldeck 0 pos);(Array.sub fulldeck (pos+1) (Array.length fulldeck))]) 
-      (Array.of_list ((fulldeck.(pos)::(Array.to_list acc)))
+    let pos = Random.int (Array.length fulldeck) in 
+    shuffle (Array.concat [(Array.sub fulldeck 0 pos);(Array.sub fulldeck (pos+1) ((Array.length fulldeck)-pos-1))]) 
+      (Array.of_list (fulldeck.(pos)::(Array.to_list acc)))
 
-       let rec deal (deck:deck) (hand:hand) = 
-         if Array.length hand = 2 then hand else
-           let pos = Random.int (Array.length deck) in 
-           if Array.mem (Array.get deck pos) hand then deal deck hand
-           else deal deck (Array.of_list ((Array.get deck pos)::(Array.to_list hand)))
+let rec deal (deck:deck) (hand:hand) : deck * hand = 
+  if Array.length hand = 2 then (deck, hand) else
+    deal (Array.sub deck 1 (Array.length deck-1)) 
+      (Array.of_list (deck.(0)::(Array.to_list hand)))
+
+let rec flop (deck:deck) (table:table) : deck * table = 
+  if Array.length table = 3 then (deck, table) else
+    flop (Array.sub deck 1 (Array.length deck-1)) 
+      (Array.of_list (deck.(0)::(Array.to_list table)))
+
+let turn (deck:deck) (table:table) : deck * table = 
+  ((Array.sub deck 1 1),
+   (Array.of_list (deck.(0)::(Array.to_list table))))
+
+let river (deck:deck) (table:table) : table = 
+  (Array.of_list (deck.(0)::(Array.to_list table)))
+
+
