e0eec51d03fa1880bb50c44139f24b9de34ef212
Jacob Levy <jal477@cornell.edu>
2020-04-19 14:29:37 -0500

    flush function done

--- poker.ml
+++ poker.ml
@@ -4,9 +4,25 @@ type card =
   |Clubs of int 
   |Spades of int
 
+type besthand = 
+  |RoyalFlush
+  |StraightFlush of int
+  |FourOfKind of int
+  |FullHouse of int * int
+  |Flush of int *  int * int * int * int
+  |Straight of int
+  |ThreeOfKind of int
+  |TwoPair of int * int * int
+  |Pair of int * int * int * int
+  |HighCard of int *  int * int * int * int
+
+
 type deck = card array
 type hand = card array
 type table = card array
+type allseven = card array
+
+
 
 let full_deck = Array.of_list [Hearts 0 ; Hearts 1 ; Hearts 2 ; Hearts 3 ; Hearts 4 ; Hearts 5 ; Hearts 6
                               ; Hearts 7 ; Hearts 8 ; Hearts 9 ; Hearts 10 ; Hearts 11 ; Hearts 12 ;
@@ -40,4 +56,38 @@ let turn (deck:deck) (table:table) : deck * table =
 let river (deck:deck) (table:table) : table = 
   (Array.of_list (deck.(0)::(Array.to_list table)))
 
+let rec insert_freq_list v acc = 
+  match acc with 
+  | [] -> ((v, 1)::acc)
+  |(v1,c)::t -> if v = v1 then (v, c+1)::t else 
+      (v1, c)::(insert_freq_list v t)
+
+let rec suits_in_hand (c:allseven) (acc:(string * int) list)= 
+  match Array.to_list c with 
+  |[] -> acc
+  |h::t -> begin 
+      match h with
+      | Hearts _ -> suits_in_hand (Array.of_list t) (insert_freq_list "h" acc)
+      | Spades _ -> suits_in_hand (Array.of_list t) (insert_freq_list "s" acc)
+      | Diamonds _ -> suits_in_hand (Array.of_list t) (insert_freq_list "d" acc)
+      | Clubs _ -> suits_in_hand (Array.of_list t) (insert_freq_list "c" acc)
+    end
+
+let rec values_in_hand (c:allseven) acc= 
+  match Array.to_list c with 
+  |[] -> acc
+  |h::t -> begin 
+      match h with
+      | Hearts i -> values_in_hand (Array.of_list t) (insert_freq_list i acc)
+      | Spades i -> values_in_hand (Array.of_list t) (insert_freq_list i acc)
+      | Diamonds i -> values_in_hand (Array.of_list t) (insert_freq_list i acc)
+      | Clubs i -> values_in_hand (Array.of_list t) (insert_freq_list i acc)
+    end
+
+let rec is_flush (c:allseven) = 
+  match suits_in_hand c [] with
+  |[] -> false
+  |(k,v)::(k1, v1)::(k2, v2)::t -> if v >= 5 || v1 >= 5 || v2 >= 5 then true else false
+  |(k,v)::(k1, v1)::t -> if v >= 5 || v1 >= 5 then true else false
+  |(k,v)::t -> if v >= 5 then true else false
 
