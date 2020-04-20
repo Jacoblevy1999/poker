510c254679166b9890d2751f71aa51160969f203
Jacob Levy <jal477@cornell.edu>
2020-04-20 00:51:11 -0500

    almost done with win conditions

--- poker.ml
+++ poker.ml
@@ -1,8 +1,4 @@
-type card =
-  |Hearts of int
-  |Diamonds of int
-  |Clubs of int 
-  |Spades of int
+type card = (int * string)
 
 type besthand = 
   |RoyalFlush
@@ -20,18 +16,20 @@ type besthand =
 type deck = card array
 type hand = card array
 type table = card array
-type allseven = card array
+type allseven = card list
 
 
 
-let full_deck = Array.of_list [Hearts 0 ; Hearts 1 ; Hearts 2 ; Hearts 3 ; Hearts 4 ; Hearts 5 ; Hearts 6
-                              ; Hearts 7 ; Hearts 8 ; Hearts 9 ; Hearts 10 ; Hearts 11 ; Hearts 12 ;
-                               Diamonds 0 ; Diamonds 1 ; Diamonds 2 ; Diamonds 3 ; Diamonds 4 ; Diamonds 5 ; Diamonds 6
-                              ; Diamonds 7 ; Diamonds 8 ; Diamonds 9 ; Diamonds 10 ; Diamonds 11 ; Diamonds 12 ;
-                               Spades 0 ; Spades 1 ; Spades 2 ; Spades 3 ; Spades 4 ; Spades 5 ; Spades 6
-                              ; Spades 7 ; Spades 8 ; Spades 9 ; Spades 10 ; Spades 11 ; Spades 12 ;
-                               Clubs 0 ; Clubs 1 ; Clubs 2 ; Clubs 3 ; Clubs 4 ; Clubs 5 ; Clubs 6
-                              ; Clubs 7 ; Clubs 8 ; Clubs 9 ; Clubs 10 ; Clubs 11 ; Clubs 12 ;]
+let full_deck = Array.of_list [(0, "h");(1, "h");(2, "h");(3, "h");(4, "h");
+                               (5, "h");(6, "h");(7, "h");(8, "h");(9, "h");
+                               (10, "h");(11, "h");(12, "h");(0, "d");(1, "d");
+                               (2, "d");(3, "d");(4, "d");(5, "d");(6, "d");
+                               (7, "d");(8, "d");(9, "d");(10, "d");(11, "d");
+                               (12, "d");(0, "c");(1, "c");(2, "c");(3, "c");(4, "c");
+                               (5, "c");(6, "c");(7, "c");(8, "c");(9, "c");
+                               (10, "c");(11, "c");(12, "c");(0, "s");(1, "s");(2, "s");
+                               (3, "s");(4, "s");(5, "s");(6, "s");(7, "s");(8, "s");(9, "s");
+                               (10, "s");(11, "s");(12, "s")]
 
 let rec shuffle (fulldeck: card array) (acc:deck) : deck = 
   if Array.length acc = 9 then acc else
@@ -56,38 +54,114 @@ let turn (deck:deck) (table:table) : deck * table =
 let river (deck:deck) (table:table) : table = 
   (Array.of_list (deck.(0)::(Array.to_list table)))
 
-let rec insert_freq_list v acc = 
-  match acc with 
-  | [] -> ((v, 1)::acc)
-  |(v1,c)::t -> if v = v1 then (v, c+1)::t else 
-      (v1, c)::(insert_freq_list v t)
+let rec freq elt lst acc= 
+  match lst with 
+  | [] -> acc
+  |h::t -> if h = elt then freq elt t (acc+1) else freq elt t acc
 
-let rec suits_in_hand (c:allseven) (acc:(string * int) list)= 
-  match Array.to_list c with 
+let suits_in_hand (c:allseven)= 
+  snd (List.split c)
+
+let rec values_in_hand (c:allseven)= 
+  fst (List.split c)
+
+let rec minimum lst acc= 
+  match lst with 
   |[] -> acc
-  |h::t -> begin 
-      match h with
-      | Hearts _ -> suits_in_hand (Array.of_list t) (insert_freq_list "h" acc)
-      | Spades _ -> suits_in_hand (Array.of_list t) (insert_freq_list "s" acc)
-      | Diamonds _ -> suits_in_hand (Array.of_list t) (insert_freq_list "d" acc)
-      | Clubs _ -> suits_in_hand (Array.of_list t) (insert_freq_list "c" acc)
-    end
+  |h::t -> if acc=0 then minimum t h else if h<acc then minimum t h
+    else minimum t acc
 
-let rec values_in_hand (c:allseven) acc= 
-  match Array.to_list c with 
+let rec replace lst old n=
+  match lst with 
+  |(k,v)::t -> if k = old then n::t else (k,v)::(replace t old n)
+  |_ -> failwith "not in list"
+
+let rec highest_n n c acc= 
+  match c with
+  | [] -> acc
+  | (k,v)::t -> if List.length acc < n then highest_n n t ((k,v)::acc) else
+      let m = minimum (values_in_hand acc) 0 in 
+      if k < m then highest_n n t (replace acc m (k,v)) else highest_n n t acc
+
+let rec add_all_value v (c:allseven) acc=
+  match c with
   |[] -> acc
-  |h::t -> begin 
-      match h with
-      | Hearts i -> values_in_hand (Array.of_list t) (insert_freq_list i acc)
-      | Spades i -> values_in_hand (Array.of_list t) (insert_freq_list i acc)
-      | Diamonds i -> values_in_hand (Array.of_list t) (insert_freq_list i acc)
-      | Clubs i -> values_in_hand (Array.of_list t) (insert_freq_list i acc)
-    end
+  |(k1,v1)::t -> if k1 = v then add_all_value v t ((k1,v1)::acc) else
+      add_all_value v t acc
 
-let rec is_flush (c:allseven) = 
-  match suits_in_hand c [] with
-  |[] -> false
-  |(k,v)::(k1, v1)::(k2, v2)::t -> if v >= 5 || v1 >= 5 || v2 >= 5 then true else false
-  |(k,v)::(k1, v1)::t -> if v >= 5 || v1 >= 5 then true else false
-  |(k,v)::t -> if v >= 5 then true else false
+let rec add_all_not_value v (c:allseven) acc=
+  match c with
+  |[] -> acc
+  |(k1,v1)::t -> if k1 <> v then add_all_not_value v t ((k1,v1)::acc) else
+      add_all_not_value v t acc
+
+let rec add_all_suit s (c:allseven) acc=
+  match c with
+  |[] -> acc
+  |(k,v)::t -> if v = s then add_all_suit s t ((k,v)::acc) else
+      add_all_suit s t acc
+
+let rec best_flush (c:allseven) (acc)= 
+  match c with
+  |[] -> acc
+  |(k,v)::t -> if freq v (suits_in_hand c) 0 < 5 then best_flush t acc
+    else if freq v (suits_in_hand c) 0 = 5 then add_all_suit v c [] else
+      highest_n 5 (add_all_suit v c []) []
+
+let rec best_four_kind (c:allseven) (acc)=
+  match c with
+  |[] -> acc
+  |(k,v)::t -> if freq k (values_in_hand c) 0 < 4 then best_four_kind t acc
+    else (add_all_value k c [])@(highest_n 1 (add_all_not_value k c []) [])
+
+let best_straight (c:allseven) (acc)=
+  let rec straight_sorter lst acc = 
+    match lst with
+    |[] -> if List.length acc >= 5 then (Array.to_list (Array.sub (Array.of_list acc) 0 5))
+      else []
+    |h::t -> if acc = [] then straight_sorter t [h] else
+      if h = 1+(List.hd acc) then straight_sorter t (h::acc) else straight_sorter t [h]
+  in straight_sorter (List.sort_uniq compare (values_in_hand c)) []
+
+let best_straight_flush c = 
+  if best_flush c [] <> [] && best_straight c [] <> [] then best_straight c [] else []
+
+let is_royal_flush c = 
+  if best_straight_flush c <> [] && List.hd (best_straight_flush c) = 12 then true else false
+
+let helper_comp (e1) (e2) = 
+  match e1, e2 with
+  |(k1, v1), (k2, v2) -> if k1 < k2 then 1 else if k1 > k2 then -1 else 0
+
+let rec three_kind c acc = 
+  match c with
+  |[] -> []
+  |(k,v)::t -> if freq k (values_in_hand c) 0 < 3 then three_kind t acc
+    else let l = List.merge helper_comp (add_all_value k c []) (three_kind t [])
+      in (List.nth l 0)::(List.nth l 1)::(List.nth l 2)::[]
+
+let rec pairs c acc = 
+  match c with
+  |[] -> []
+  |(k,v)::t -> if (freq k (values_in_hand c) 0) < 2 then pairs t acc
+    else let l = List.merge helper_comp (add_all_value k c []) (pairs t [])
+      in if List.length l > 4 then 
+        (List.nth l 0)::(List.nth l 1)::(List.nth l 2)::(List.nth l 3)::[] else
+        l
+
+let two_pair c = 
+  let p = pairs c [] in
+  if List.length p >= 4 then p else []
+
+let one_pair c = 
+  let p = pairs c [] in
+  if List.length p = 2 then (List.nth p 0)::(List.nth p 1)::[] else
+    []
+
+let best_fh c = 
+  let tk = (three_kind c []) in
+  if tk <> [] && one_pair (add_all_not_value (fst (List.hd tk)) c []) <> [] then
+    let p = one_pair (add_all_not_value (fst (List.hd tk)) c []) in 
+    tk@p 
+  else []
 
