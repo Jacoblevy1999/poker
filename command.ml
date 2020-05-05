0b06bca2023990acf72cbc7b9e3dfd5734a7f550
Wyatt Marshall <wgm46@cornell.edu>
2020-05-05 18:16:38 -0400

    Fixed $ issue

--- command.ml
+++ command.ml
@@ -60,10 +60,11 @@ let rec is_phrase_wellformed strlst =
     by an integer and returns the integer as type int. 
     Example: [remove_dsign "$25"] returns [25]. *)
 let remove_dsign str = 
-  let list = String.split_on_char '$' str in 
-  match list with 
-  |""::[t] -> int_of_string t 
-  |_ -> -1
+  try int_of_string str with (Failure "") -> 
+    let list = String.split_on_char '$' str in 
+    match list with 
+    |""::[t] -> int_of_string t 
+    |_ -> -1
 
 let parse str = 
   let move = str_to_strlst str in
